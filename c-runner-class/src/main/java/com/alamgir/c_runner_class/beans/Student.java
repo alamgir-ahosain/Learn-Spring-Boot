4a556c9af8d170be534e4344016546c29270b242
alamgir-ahosain <alamgir.ahosain@gmail.com>
2025-09-23 00:03:03 +0600

    creating  Bean

---
+++ c-runner-class/src/main/java/com/alamgir/c_runner_class/beans/Student.java
@@ -0,0 +1,11 @@
+package com.alamgir.c_runner_class.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+
+    public Student() {
+        System.out.println("student bean is created...");
+    }
+}
