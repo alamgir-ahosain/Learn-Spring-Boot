6614874826804de07a0bbb2f13e2f2365864f045
alamgir-ahosain <alamgir.ahosain@gmail.com>
2025-09-23 00:02:05 +0600

    Main class

---
+++ c-runner-class/src/test/java/com/alamgir/c_runner_class/CRunnerClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alamgir.c_runner_class;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CRunnerClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
