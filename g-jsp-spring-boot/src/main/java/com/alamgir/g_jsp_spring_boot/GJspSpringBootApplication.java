35731bf63f072b9037362516c550ef4e9717af8f
alamgir-ahosain <alamgir.ahosain@gmail.com>
2025-10-01 13:56:06 +0600

    main class

---
+++ g-jsp-spring-boot/src/main/java/com/alamgir/g_jsp_spring_boot/GJspSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.alamgir.g_jsp_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GJspSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GJspSpringBootApplication.class, args);
+	}
+
+}
