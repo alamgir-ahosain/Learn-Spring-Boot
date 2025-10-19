eed38e1a3f39b9404a06573e8a936234b18ca493
alamgir-ahosain <alamgir.ahosain@gmail.com>
2025-10-20 00:18:23 +0600

    main class

---
+++ i-consuming-rest-api/src/test/java/com/alamgir/i_consuming_rest_api/IConsumingRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alamgir.i_consuming_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IConsumingRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
