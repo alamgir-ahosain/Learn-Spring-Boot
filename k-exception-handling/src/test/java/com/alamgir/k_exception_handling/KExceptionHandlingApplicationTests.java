e27c34a392563dc34e17c028e237458e46be1f6a
alamgir-ahosain <alamgir.ahosain@gmail.com>
2025-10-21 13:36:36 +0600

    Main Class

---
+++ k-exception-handling/src/test/java/com/alamgir/k_exception_handling/KExceptionHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alamgir.k_exception_handling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KExceptionHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
