c5ffbbb4394a2a96a8b07e8d59d916558d12e91e
alamgir-ahosain <alamgir.ahosain@gmail.com>
2025-10-24 16:51:50 +0600

    Main class

---
+++ l-spring-boot-security-jwt/src/test/java/com/alamgir/l_spring_boot_security_jwt/LSpringBootSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alamgir.l_spring_boot_security_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LSpringBootSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
